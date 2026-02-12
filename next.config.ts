b8728650625807e6fa24b0e1d3a4060ffc1acce4
devinjnugraha <devin.nugraha@cgsi.com>
2026-02-12 16:39:31 +0700

    fix: prefetch link

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	output: 'export',
 	basePath: '/litools',
-	distDir: 'out/litools'
+	distDir: 'out/litools',
 	images: { unoptimized: true },
 };
 
